58510bf1fe65ed776f791b3f627b6b563792172a
Neeraj-vimla-goyal <127337401+Neeraj-vimla-goyal@users.noreply.github.com>
2023-03-11 19:03:18 +0530

    code optimization, change in function argument
    code optimization, change in function argument

--- function.py
+++ function.py
@@ -1,9 +1,18 @@
-def read_todo(file_path="files/todo.txt"):
+FILEPATH = "files/todo.txt"
+
+
+def read_todo(file_path=FILEPATH):
+    """
+    this function will read the text file and return the todo items list
+    """
     with open(file_path, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def write_todo(todo_args, file_path="files/todo.txt"):
+def write_todo(todo_args, file_path=FILEPATH):
+    """
+    this function will write todo items into text file
+    """
     with open('files/todo.txt', 'w') as file:
         file.writelines(todo_args)
